32b7a27cdbc5ed2a5a5065e5bca0432b3df8f5dd
Dragmine149 <dan@thebanners.uk>
2026-07-15 20:23:46 +0100

    Image support (not via icon) and move description to tooltips

--- crates/frontend/src/apworld.rs
+++ crates/frontend/src/apworld.rs
@@ -1,11 +1,9 @@
 use gpui::{
-    App, AppContext, Context, Entity, Image, ImageSource, IntoElement, ObjectFit, ParentElement,
-    Render, RenderImage, Styled, StyledImage, SvgRenderer, Window, div, img,
+    App, AppContext, Context, Entity, ImageSource, InteractiveElement, IntoElement, ParentElement,
+    Render, StatefulInteractiveElement, Styled, Window, img, prelude::FluentBuilder,
 };
-use gpui_component::{Icon, IconName, Sizable, button::Button, label::Label, v_flex};
+use gpui_component::{button::Button, label::Label, tooltip::Tooltip, v_flex};
 use rustipelago_schema::archipelago::ApCard;
-
-use crate::{Assets, client::open_client};
 
 pub(crate) struct APWorldCard {
     pub world_info: ApCard,
@@ -20,29 +18,27 @@ impl APWorldCard {
 }
 
 impl Render for APWorldCard {
-    fn render(&mut self, window: &mut Window, cx: &mut Context<Self>) -> impl IntoElement {
+    fn render(&mut self, _window: &mut Window, cx: &mut Context<Self>) -> impl IntoElement {
+        let description = self.world_info.description.clone();
         Button::new("ap-world")
             .child(
                 v_flex()
+                    .id("apcard")
                     .child(
-                        Icon::default()
-                            .path("ArchipelagoIcon.svg")
-                            .size_full()
-                            .pb_10(),
-                        // img(self
-                        //     .world_info
-                        //     .icon
-                        //     .clone()
-                        //     .unwrap_or("assets/archipelago_icon.png".to_string()))
-                        // .with_fallback(|| div().child("Failed loading image").into_any_element())
-                        // .object_fit(ObjectFit::Contain),
+                        img(ImageSource::from("images/ArchipelagoIcon.png"))
+                            .size_20()
+                            .self_center(),
                     )
                     .child(
                         Label::new(self.world_info.name.clone())
                             .text_xl()
                             .text_center(),
                     )
-                    .child(Label::new(self.world_info.description.clone()).text_center()),
+                    .when(description.len() > 0, |this| {
+                        this.tooltip(move |window, cx| {
+                            Tooltip::new(description.clone()).build(window, cx)
+                        })
+                    }),
             )
             .on_click(cx.listener(|this, ev, win, cx| {
                 println!("Clicked world: {}", this.world_info.name);
